3ef65b4c0c499c0cbb4201f55cf6596763e57f3f
markusmatthys <markus.matthys@outlook.com>
2020-05-15 09:37:51 +0200

    New Share Feature

---
+++ Finance.Android/Dependencies/Share.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.Content;
+using Finance.Droid.Dependencies;
+using Finance.Interfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Share))]
+namespace Finance.Droid.Dependencies
+{
+    public class Share : IShare
+    {
+        public Task Show(string title, string url)
+        {
+            var intent = new Intent(Intent.ActionSend);
+            intent.SetType("text/plain");
+
+            intent.PutExtra(Intent.ExtraText, url);
+            intent.PutExtra(Intent.ExtraSubject, title);
+
+            Intent chooserIntent = Intent.CreateChooser(intent, title);
+            chooserIntent.SetFlags(ActivityFlags.NewTask);
+           
+            Android.App.Application.Context.StartActivity(chooserIntent);
+
+            return Task.FromResult(true);
+        }
+    }
+}
+
